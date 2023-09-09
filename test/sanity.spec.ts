ce8759a8c90215b8cbacbd5a8738605144109e50
Matheus Rabelo <matheusfreirerabelo@gmail.com>
2023-09-09 00:35:52 -0300

    Add tests

---
+++ test/sanity.spec.ts
@@ -0,0 +1,11 @@
+import { expect, test, describe } from "bun:test";
+
+describe("Sanity", () => {
+  test("2 + 2", () => {
+    expect(2 + 2).toBe(4);
+  });
+
+  test("2 * 2", () => {
+    expect(2 * 2).toBe(4);
+  });
+});
